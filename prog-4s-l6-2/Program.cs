490d91d261058d48db223ae1ed946575518efa14
oldkirillius <49608020+oldkirillius@users.noreply.github.com>
2019-05-20 14:03:29 +0300

    сообщение фиксации

---
+++ prog-4s-l6-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_4s_l6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
